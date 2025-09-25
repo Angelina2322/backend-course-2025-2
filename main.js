141c98be8fe8112e0927a9b63315fb2afa017a8a
Angelina2322 <apavlushce@gmail.com>
2025-09-25 12:38:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // main.js
 
-// Підключаємо модуль URL (вбудований в Node.js)
-const { URL, URLSearchParams } = require('url');
+const { URL } = require('url');
 
 // Константи для варіанту 7
-const date = "20230201";      // дата для запиту
-const time_period = "m";      // період (month)
+const date = "20230201";
+const time_period = "m";
 
 // Базовий URL сервера НБУ
 const baseURL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/inflation";
@@ -16,7 +15,17 @@ const url = new URL(baseURL);
 // Додаємо параметри через SearchParams
 url.searchParams.append("date", date);
 url.searchParams.append("period", time_period);
-url.searchParams.append("json", "true"); 
+url.searchParams.append("json", "true");   // json=true
 
 // Виводимо URL у консоль
 console.log(url.toString());
+
+// --- Перевірка URL ---
+const fetch = require('node-fetch'); // якщо Node.js < 18, встанови: npm install node-fetch
+
+fetch(url.toString())
+  .then(res => res.json())
+  .then(data => {
+    console.log(data); // виведе дані від сервера
+  })
+  .catch(err => console.error(err));
